7523f946c4ba5a9bdd79e32165494f7c3b3a9106
nakanoyuki <yukich41@gmail.com>
2025-04-22 16:40:59 +0900

    mapを利用しコード量削減

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -5,12 +5,39 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { registerSchema, RegisterFormInputs } from "../schemas/registerSchema";
 import { sendRegisterRequest } from "../lib/api";
+import { TextInput } from "./form/TextInput";
+import { FormError } from "./form/FormError";
+
+const formfields = [
+  {
+    name: "name",
+    label: "名前",
+    placeholder: "名前",
+  },
+  {
+    name: "email",
+    label: "メールアドレス",
+    placeholder: "メールアドレス",
+  },
+  {
+    name: "password",
+    label: "パスワード",
+    placeholder: "パスワード",
+    type: "password",
+  },
+  {
+    name: "confirmPassword",
+    label: "パスワード確認",
+    placeholder: "パスワード確認",
+    type: "password",
+  },
+] as const;
 
 export const RegisterForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
   } = useForm<RegisterFormInputs>({
     resolver: zodResolver(registerSchema),
   });
@@ -22,41 +49,18 @@ export const RegisterForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <input {...register("name")} placeholder="名前" className="border p-2" />
-      {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+      {formfields.map((field) => (
+        <div key={field.name}>
+          <TextInput
+            {...register(field.name)}
+            label={field.label}
+            placeholder={field.label}
+          />
+          <FormError errormessage={errors[field.name]?.message} />
+        </div>
+      ))}
 
-      <input
-        {...register("email")}
-        placeholder="メールアドレス"
-        className="border p-2"
-      />
-      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-
-      <input
-        type="password"
-        {...register("password")}
-        placeholder="パスワード"
-        className="border p-2"
-      />
-      {errors.password && (
-        <p className="text-red-500">{errors.password.message}</p>
-      )}
-
-      <input
-        type="password"
-        {...register("confirmPassword")}
-        placeholder="パスワード確認"
-        className="border p-2"
-      />
-      {errors.confirmPassword && (
-        <p className="text-red-500">{errors.confirmPassword.message}</p>
-      )}
-
-      <button
-        type="submit"
-        disabled={isSubmitting}
-        className="bg-blue-500 text-white px-4 py-2"
-      >
+      <button type="submit" className="bg-blue-500 text-white px-4 py-2">
         登録
       </button>
     </form>
